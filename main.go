9943b9654d2b7920dc35daf42a9d410103260fae
vomolo <valentineomolo1@gmail.com>
2025-02-12 14:17:33 +0300

    [EDIT]: Add about, contact, and services handler.

--- main.go
+++ main.go
@@ -6,11 +6,26 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	fmt.Fprintf(w, "Welcome to the home page!")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the about page!")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the contact page!")
+}
+
+func servicesHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the services page!")
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/services", servicesHandler)
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
